fca7aa011fc009a193132e7589eaaa85098adf32
ManishKumarITG <manishkumar@itgeeks.com>
2025-10-17 20:02:36 +0530

    initail stage

---
+++ The-Teacher/src/App.jsx
@@ -0,0 +1,14 @@
+import React , { useState } from 'react'
+import './App.css'
+import Header from './Components/Header'
+
+function App() {
+ 
+  return (
+    <>
+       <Header/>
+    </>
+  )
+}
+
+export default App
